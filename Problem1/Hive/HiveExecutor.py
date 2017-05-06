20be0cf68e8c31cdc6cae9e31aaf1d03f8ad24ec
nitinchoudhry24 <nitinchoudhry@icloud.com>
2017-05-06 21:22:30 +0530

    Hive Quesry Executor added

---
+++ Problem1/Hive/HiveExecutor.py
@@ -0,0 +1,72 @@
+'''
+Created on 05-May-2017
+@author: nitinchoudhry
+'''
+from Resources.Logger import *
+import pyhs2
+
+
+class HiveExecutor(object):
+    
+    
+    def __init__(self,hiveserver="",port="10000",user="hive",paswword="hive@123",dbname="default",QueryFile="../Configs/HiveQueries"):
+        
+        self.QueryFile=QueryFile
+        self.hiveServer = hiveserver
+        self.port = port
+        self.user = user
+        self.password = paswword
+        self.dbname = dbname
+        Connection=pyhs2.connect(host=self.hiveServer,
+                                  port=self.port,
+                                  authMechanism="PLAIN",
+                                  user=self.user,
+                                  password=self.password,
+                                  database=self.dbname)
+        self.Cursor=Connection.cursor()
+        
+        
+        
+        
+    
+    def Run(self):
+        
+        logs("../Output/HiveLogs")
+        self.logger=Initiate_logger("HiveLogs")
+        Lines=self.QueryFile.readlines()
+        try:
+            for line in Lines:
+                self.RunHiveCommand(line.strip("\n"))
+        
+            self.Cursor.close()
+        except Exception,e:
+            self.logger.error("Exception Encountered while Running Hive Queries")
+            self.logger.error("Exception:"+str(e))
+        
+    def RunHiveCommand(self,Query):
+        
+        self.logger.info("Executing Query :"+Query)
+        
+        
+        try:
+            self.Cursor.execute(Query)
+            self.logger.info("#######################Result#################")
+            Result=""
+            for i in self.Cursor.fetch():
+                Result+= i+"\n"
+        
+                             
+            self.logger.info(Result)
+            
+        except Exception,e:
+            self.logger.error("Failure While Execution of Query")
+            self.logger.error(str(e))
+            
+        
+        
+        
+            
+    
+    
+        
+        
---
+++ Problem1/Hive/HiveQL.py
@@ -0,0 +1,76 @@
+"""
+author="Nitin Choudhry"
+
+"""
+from Resources import pyhs2
+
+class hivetable:
+    
+    
+    
+        def __init__(self,hiveserver="",port="10000",user="hive",paswword="hive@123",dbname="default",logger):
+            self.hiveServer = hiveserver
+            self.port = port
+            self.user = user
+            self.password = paswword
+            self.dbname = dbname
+            self.Logger=logger
+        
+        def getpartitionstring(self,parts):
+                retstr=""
+                for key in parts:
+                        lit=parts[key]
+                        if(lit.replace("-","").replace(".","").isdigit()):
+                            retstr+=key+"="+lit+","
+                        else:
+                            retstr+=key+"=\'"+parts[key]+"\',"
+                return retstr[:-1]
+
+        def getpartitioncols(self,parts):
+                retstr=""
+                for key in parts:
+                        retstr+=key+" "+parts[key]+","
+                return retstr[:-1]
+
+        def runHiveCommand(self, queries):
+                status = 0
+                try:
+                    with pyhs2.connect(host=self.hiveServer,
+                                  port=self.port,
+                                  authMechanism="PLAIN",
+                                  user=self.user,
+                                  password=self.password,
+                                  database=self.dbname) as conn:
+                        with conn.cursor() as cur:
+                            queryList = []
+                            queryList = queries.split(";")
+                            for query in queryList:
+                                if query != '' and query != ' ':   
+                                 
+                                    try:
+                                        self.Logger.info("Executing Query")
+                                        cur.execute(query)
+                                    except Exception as e:
+                                        status = e.errorCode
+                                        print ("Hive Query Failed:"+query)
+                                        print(e.errorMessage)
+                                else:
+                                    print ("Hive Query Successful")
+                except:
+                    print ("Hive Query Failed:"+query)
+                    status = 1 
+                return status
+
+
+        def create(self,table,serialclass,fileformat,partitionkeys, hiveServer):
+                self.hiveServer = hiveServer
+
+                query="CREATE EXTERNAL TABLE "+table+" PARTITIONED BY ("+self.getpartitioncols(partitionkeys)
+                query+=") ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.thrift.ThriftDeserializer'"
+                query+=" WITH SERDEPROPERTIES(\'serialization.format\'=\'"
+                query+=fileformat+"\',\'serialization.class\'=\'"
+                query+=serialclass+"\') STORED AS SEQUENCEFILE;"
+
+                self.runHiveCommand (query)
+
+        
